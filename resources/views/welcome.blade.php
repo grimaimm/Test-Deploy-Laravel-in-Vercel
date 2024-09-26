020325ab7441bbbdd60867c484909c636c065109
grimaim <muhammadrahim.mr196@gmail.com>
2024-09-26 23:23:10 +0700

    test: conf react

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,10 +9,10 @@
   @vite(['resources/js/app.jsx', 'resources/css/app.css']) --}}
   {{-- <script type="module" src="/build/assets/app-DD7HAtTv.js"></script> --}}
 
-  <link rel="preload" as="style" href="{{ '/build/assets/app-DElv8RGW.css' }}">
-  <link rel="modulepreload" href="{{ '/build/assets/app-DD7HAtTv.js' }}">
-  <link rel="stylesheet" href="{{ '/build/assets/app-DElv8RGW.css' }}">
-  <script type="module" src="{{ '/build/assets/app-DD7HAtTv.js' }}"></script>
+  <link rel="preload" as="style" href="{{ 'https://test-deploy-laravel-in-vercel.vercel.app/build/assets/app-DElv8RGW.css' }}">
+  <link rel="modulepreload" href="{{ 'https://test-deploy-laravel-in-vercel.vercel.app/build/assets/app-DD7HAtTv.js' }}">
+  <link rel="stylesheet" href="{{ 'https://test-deploy-laravel-in-vercel.vercel.app/build/assets/app-DElv8RGW.css' }}">
+  <script type="module" src="{{ 'https://test-deploy-laravel-in-vercel.vercel.app/build/assets/app-DD7HAtTv.js' }}"></script>
 </head>
 
 <body>
