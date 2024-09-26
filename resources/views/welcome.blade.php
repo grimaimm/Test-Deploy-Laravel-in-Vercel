dfd252a9894c91b2af55b3dfd4468f1e331c58fb
grimaim <muhammadrahim.mr196@gmail.com>
2024-09-26 23:32:10 +0700

    test: conf react

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,22 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Laravel with React</title>
-  {{-- @viteReactRefresh
-  @vite(['resources/js/app.jsx', 'resources/css/app.css']) --}}
-  {{-- <script type="module" src="/build/assets/app-DD7HAtTv.js"></script> --}}
-
-  <link rel="preload" as="style" href="{{ 'https://test-deploy-laravel-in-vercel.vercel.app/build/assets/app-DElv8RGW.css' }}">
-  <link rel="modulepreload" href="{{ 'https://test-deploy-laravel-in-vercel.vercel.app/build/assets/app-DD7HAtTv.js' }}">
-  <link rel="stylesheet" href="{{ 'https://test-deploy-laravel-in-vercel.vercel.app/build/assets/app-DElv8RGW.css' }}">
-  <script type="module" src="{{ 'https://test-deploy-laravel-in-vercel.vercel.app/build/assets/app-DD7HAtTv.js' }}"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel with React</title>
+    @viteReactRefresh
+    @vite(['resources/js/app.jsx', 'resources/css/app.css'])
 </head>
-
 <body>
-  <div id="app"></div>
+    <div id="app"></div>
 </body>
-
 </html>
