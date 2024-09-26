3c2c4bca1b19fa779642a7af0bf9b19c80c3b054
grimaim <muhammadrahim.mr196@gmail.com>
2024-09-26 23:19:33 +0700

    test: conf react

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel with React</title>
-    @viteReactRefresh
-    @vite(['resources/js/app.jsx', 'resources/css/app.css'])
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Laravel with React</title>
+  {{-- @viteReactRefresh
+  @vite(['resources/js/app.jsx', 'resources/css/app.css']) --}}
+  {{-- <script type="module" src="/build/assets/app-DD7HAtTv.js"></script> --}}
+
+  <link rel="preload" as="style" href="{{ '/build/assets/app-DElv8RGW.css' }}">
+  <link rel="modulepreload" href="{{ '/build/assets/app-DD7HAtTv.js' }}">
+  <link rel="stylesheet" href="{{ '/build/assets/app-DElv8RGW.css' }}">
+  <script type="module" src="{{ '/build/assets/app-DD7HAtTv.js' }}"></script>
 </head>
+
 <body>
-    <div id="app"></div>
+  <div id="app"></div>
 </body>
+
 </html>
